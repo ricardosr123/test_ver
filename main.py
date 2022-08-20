e9df50c1dc94afd8672b414400da5c231da31f52
ricardosr123 <110127389+ricardosr123@users.noreply.github.com>
2022-08-20 18:27:14 -0500

    get json file

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 import pandas as pd
 import re
+import json
 
 # Next regex look for the structure "(CO.REG". It considers several cases:
 # - With/without first parenthesis
 # - Spaces between parenthesis and CO, and between C0 . and . Reg
-regex1base = '^\(?\s?CO\s?.?\s?REG'
+rgx_coreg_base = '^\(?\s?CO\s?.?\s?REG'
 # - Letters misspelled.
 variations1 = '[A-Z0-9\(\)\[\]\{\}]'
-regex1lst = [''.join([regex1base[0:i], variations1, regex1base[i + 1:]]) for i in [7, 8, 17, 18, 19]]
-regex1lst.insert(0, '^\(CO.REG')  # "Perfect" pattern
+rgx_coreg_lst = [''.join([rgx_coreg_base[0:i], variations1, rgx_coreg_base[i + 1:]]) for i in [7, 8, 17, 18, 19]]
+rgx_coreg_lst.insert(0, '^\(CO.REG')  # "Perfect" pattern
 # - Some additional cases selected at random
-regex1lst.append('^\(CO')
-regex1lst.append('R.G.*:')
-regex1lst.append('C.+EG')
-regex1 = 'r\''+'|'.join(regex1lst)+'\''
+rgx_coreg_lst.append('^\(CO')
+rgx_coreg_lst.append('R.G.*:')
+rgx_coreg_lst.append('C.+EG')
+rgx_coreg = 'r\'' + '|'.join(rgx_coreg_lst) + '\''
 
 # Next regex look for the structure "-INVOICE-". It considers several cases:
 # - With/without hyphens and possible misspelled letters
-regex2base = '-?INVOICE-?'
+rgx_invoice_base = '-?INVOICE-?'
 variations2 = '[A-Z0-9]'
-regex2lst = [''.join([regex2base[0:i], variations2, regex2base[i + 1:]]) for i in [2, 3, 4, 5, 6, 7, 8]]
-regex2lst.insert(0, '-INVOICE-')  # "Perfect" pattern
-regex2 = 'r\''+'|'.join(regex2lst)+'\''
+rgx_invoice_lst = [''.join([rgx_invoice_base[0:i], variations2, rgx_invoice_base[i + 1:]]) for i in [2, 3, 4, 5, 6, 7, 8]]
+rgx_invoice_lst.insert(0, '-INVOICE-')  # "Perfect" pattern
+rgx_invoice = 'r\'' + '|'.join(rgx_invoice_lst) + '\''
 
 # Next regex look for the structure  "TOTAL ROUNDED"
-regex3base = 'TOTAL\s?\s?ROUNDED'
-regex3lst = [''.join([regex3base[0:i], variations2, regex3base[i + 1:]]) for i in [0, 1, 2, 3, 4, 11, 12, 13, 14, 15, 16, 17]]
-regex3lst.insert(0, 'TOTAL\sROUNDED')  # "Perfect" pattern
-regex3 = 'r\''+'|'.join(regex3lst)+'\''
+rgx_totr_base = 'TOTAL\s?\s?ROUNDED'
+rgx_totr_lst = [''.join([rgx_totr_base[0:i], variations2, rgx_totr_base[i + 1:]]) for i in [0, 1, 2, 3, 4, 11, 12, 13, 14, 15, 16, 17]]
+rgx_totr_lst.insert(0, 'TOTAL\sROUNDED')  # "Perfect" pattern
+rgx_totr = 'r\'' + '|'.join(rgx_totr_lst) + '\''
 
-# regex4 = r'CHANGE'
+# rgx_change = r'CHANGE'
+rgx_date = r'[0-3][0-9]\s?-\s?[01][0-9]\s?-\s?[0-9][0-9]'
+
+rgx_item = r'ITEM\s?\(\s?S\s?\)'
 
 class TwyReceipt(object):
 
@@ -44,50 +48,46 @@ class TwyReceipt(object):
         self.receipt = pd.read_csv(ocr_file, names=['x1', 'y1', 'x2', 'y2', 'x3', 'y3', 'x4', 'y4', 'text'],
                                    engine="python", on_bad_lines=manual_separation)
 
-    def get_company(self):
+    def extract_company(self):
         complst = []
         i = 1
         while True:
             complst.append(self.receipt.iloc[i, 8])
             i += 1
-            if re.search(regex1, self.receipt.iloc[i, 8]) or i > 5:
+            if re.search(rgx_coreg, self.receipt.iloc[i, 8]) or i > 5:
                 break
         self.company = ' '.join(complst)
-        return self.company
 
-    def get_address(self):
+    def extract_address(self):
         addrlst = []
         i = 1
-        while re.search(regex1, self.receipt.iloc[i, 8]) is None and i < 5:
+        while re.search(rgx_coreg, self.receipt.iloc[i, 8]) is None and i < 5:
             i += 1
         i += 1
         while True:
             addrlst.append(self.receipt.iloc[i, 8])
             i += 1
-            if re.search(regex2, self.receipt.iloc[i, 8]) or i > 14:
+            if re.search(rgx_invoice, self.receipt.iloc[i, 8]) or i > 14:
                 break
         self.address = ' '.join(addrlst)
-        return self.address
 
-    def get_total(self):
-        i = self.receipt[self.receipt['text'].str.match(regex3)].index.values+1
+    def extract_total(self):
+        i = self.receipt[self.receipt['text'].str.match(rgx_totr)].index.values + 1
         self.tot_round = self.receipt.iloc[i, 8].str.split(expand=True).iloc[0, 1]
-        return self.tot_round
 
-    def get_date(self):
-        #metodo de chequeo:
-        #i = self.receipt[self.receipt['text'].str.match(regex4)].index.values+2
+    def extract_date(self):
+        #Alternative method:
+        #i = self.receipt[self.receipt['text'].str.match(rgx_change)].index.values+2
         #self.date = self.receipt.iloc[i, 8].str.split(expand=True).iloc[0, 0]
-        self.date = self.receipt.loc[self.receipt['text'].str.match(r'[0-3][0-9]\s?-\s?[01][0-9]\s?-\s?[0-9][0-9]'),
+        self.date = self.receipt.loc[self.receipt['text'].str.match(rgx_date),
                                      'text'].item().split()[0]
-        return self.date
 
-    def get_items(self):
+    def extract_items(self):
         #Regex a item
-        self.n_line_items = int(self.receipt.loc[self.receipt['text'].str.match(r'ITEM\s?\(\s?S\s?\)'), 'text'].item().split()[-1])
+        self.n_line_items = int(self.receipt.loc[self.receipt['text'].str.match(rgx_item), 'text'].item().split()[-1])
 
         i = 3
-        while re.search(regex2, self.receipt.iloc[i, 8]) is None and i < 15:
+        while re.search(rgx_invoice, self.receipt.iloc[i, 8]) is None and i < 15:
             i += 1
         i += 3
         self.line_items = []
@@ -101,18 +101,30 @@ class TwyReceipt(object):
             i += 1
             self.line_items[j]['total'] = self.receipt.iloc[i, 8]
             i += 3
-            #get item
-        print(self.line_items)
-        return self.n_line_items
+
+    def get_json(self,json_name):
+        self.extract_company()
+        self.extract_date()
+        self.extract_address()
+        self.extract_items()
+        self.extract_total()
+        dictionary = {'company': self.company,
+                      'date': self.date,
+                      'address': self.address,
+                      'line_items': self.line_items,
+                      'total': self.tot_round}
+        json_str = json.dumps(dictionary, indent=4)
+        with open(json_name, "w") as outfile:
+            outfile.write(json_str)
 
 def run():
     receipt_1 = TwyReceipt("./ocr_files/OCR1.txt")
     receipt_2 = TwyReceipt("./ocr_files/OCR2.txt")
     receipt_3 = TwyReceipt("./ocr_files/OCR1mod.txt")
 
-    receipt_1.get_items()
-    receipt_2.get_items()
-    receipt_3.get_items()
+    receipt_1.get_json("./output/OCR1.json")
+    receipt_2.get_json("./output/OCR2.json")
+    receipt_3.get_json("./output/OCR1mod.json")
 
 
 if __name__ == '__main__':
